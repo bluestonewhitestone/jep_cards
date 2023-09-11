47ebf4ecae66969c7624c839103a47285d4de867
bluestonewhitestone <89286297+bluestonewhitestone@users.noreply.github.com>
2023-09-11 16:53:43 -0700

    Update main.py
    Displays clue amount and addition of clue amounts after each successive turn

--- main.py
+++ main.py
@@ -27,20 +27,23 @@ with open('JEOPARDY_CSV.csv') as csv_file:
     next(csv_file)
 
     for row in csv_reader:
-        questions.append((row[5], row[6], row[3]))
+        questions.append((row[5], row[6], row[3], row[4]))
+
 
     while count != 0:
         random.shuffle(questions)
-        print(questions[0][2])
+        print(questions[0][2], "--", questions[0][3])
         print(questions[0][0])
         x = input("")
         if x == questions[0][1]:
-            print("correct")
-            total_right += 1
+            amount = questions[0][3].replace("$", "")
+            total_right += int(amount)
+            print("Correct:", total_right)
             continue
         count -= 1
         print("Unfortunately, no, the correct answer is: " + questions[0][1])
         print("Correct:", total_right)
+
 
 
 
