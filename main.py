b3435a8dc03c9a59a4b66f4356e0a3c4d991a27b
bluestonewhitestone <89286297+bluestonewhitestone@users.noreply.github.com>
2023-09-11 13:55:51 -0700

    Update main.py
    Added functionality to include category display and amount of questions answered correctly

--- main.py
+++ main.py
@@ -20,21 +20,28 @@ import random
 questions = []
 count = 1
 
+total_right = 0
+
 with open('JEOPARDY_CSV.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     next(csv_file)
 
     for row in csv_reader:
-        questions.append((row[5], row[6]))
+        questions.append((row[5], row[6], row[3]))
 
     while count != 0:
         random.shuffle(questions)
+        print(questions[0][2])
         print(questions[0][0])
         x = input("")
         if x == questions[0][1]:
             print("correct")
+            total_right += 1
             continue
         count -= 1
-        print("Unfortunately, no, correct answer is: " + questions[0][1])
+        print("Unfortunately, no, the correct answer is: " + questions[0][1])
+        print("Correct:", total_right)
+
+
 
 
